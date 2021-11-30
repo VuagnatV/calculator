50917e764a6ec2ca8f306d7a6b15b2229483e55e
VuagnatV <37271745+VuagnatV@users.noreply.github.com>
2021-11-30 23:13:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+function CalculatorButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+function Calculator() {
+  const [operation, setOperation] = useState('')
+  const [total, setTotal] = useState(0)
+
+  const operators = ['.','+', '-', '*', '/', '(', ')', '=']
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  function calculate() {
+    // Eval is evil, try to find something safer :)
+    setTotal(eval(operation))
+  }
+
+  return (
+    <div>
+      {operation}={total}
+      {[...operators, ...numbers].map(
+        (operator) =>
+          <CalculatorButton
+            value={operator}
+            //onClick={() => setOperation(operation + operator)}
+            onClick={() =>
+              operator === '=' 
+              ? calculate() 
+              : setOperation(operation + operator)
+            }
+          />
+      )}
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calculator />
     </div>
-  );
+  )
 }
 
 export default App;
